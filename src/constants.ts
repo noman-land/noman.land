697393cf6170699bc83b9117caeb3c9dc69279ca
Noman <noman@noman.land>
2025-02-12 01:27:53 -0500

    Setup fedi .well-known redirects

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,3 @@
 export const MATRIX_JSON = {
   'm.server': 'matrix.noman.land:443',
 };
-
-export const MASTODON_XML = [
-  '<?xml version="1.0" encoding="UTF-8"?>',
-  '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">',
-  '<Link rel="lrdd" type="application/xrd+xml" template="https://mastodon.noman.land/.well-known/webfinger?resource={uri}"/>',
-  '</XRD>',
-].join('');
--- src/routes/wellKnown.tsx
+++ src/routes/wellKnown.tsx
@@ -1,9 +1,8 @@
 import { Hono } from 'hono';
-import { MASTODON_XML, MATRIX_JSON } from '../constants';
+import { MATRIX_JSON } from '../constants';
 
 export const wellKnown = new Hono<{ Bindings: Bindings }>({ strict: false })
   .get('/matrix/server', async c => c.json(MATRIX_JSON))
-  .get('/host-meta', async c => {
-    c.res.headers.append('Content-Type', 'application/xml');
-    return c.newResponse(MASTODON_XML);
+  .on('GET', ['/host-meta/:path?', '/webfinger/:path?', '/nodeinfo/:path?'], async c => {
+    return c.redirect(`https://mastodon.noman.land${c.req.path}`);
   });
