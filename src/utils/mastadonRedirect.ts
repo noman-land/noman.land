b0456944bea5b961900de19c4610f7f0c205a4e8
Noman <noman@noman.land>
2025-03-07 01:40:50 -0500

    Clean up

--- src/utils/mastadonRedirect.ts
+++ src/utils/mastadonRedirect.ts
@@ -2,10 +2,8 @@ import { Context } from 'hono';
 import { MASTODON_DOMAIN, MASTODON_USER } from '../constants';
 
 export const mastodonRedirect = async (c: Context) => {
-  const url = new URL(`https://${MASTODON_DOMAIN}${c.req.path}`);
-  for (const [k, v] of Object.entries(c.req.query())) {
-    url.searchParams.append(k, v);
-  }
+  const url = new URL(c.req.url);
+  url.hostname = MASTODON_DOMAIN;
   url.searchParams.append('resource', `acct:${MASTODON_USER}@${MASTODON_DOMAIN}`);
   return c.redirect(url);
 };
